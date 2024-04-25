83d6b4f3e681d85b6a44c5ee4e8cf917298c00a7
Samius Sazin <samiussazin45@gmail.com>
2024-04-25 13:57:56 +0600

    commited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 //For MongoDB
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 //For CORS
 const cors = require('cors');
@@ -25,18 +25,88 @@ async function run() {
     try {
         // Access Database
         const database = client.db("sain_courier_service");
-        //Access Collection of sain_courier_service database
+        //Access services Collection of sain_courier_service database
         const servicesCollection = database.collection("services");
+        //Access booking_info Collection of sain_courier_service database
+        const bookingInfoCollection = database.collection("booking_info");
 
+        //return all services
         app.get('/services', async (req, res) => {
             //Cursor is like have all services
             const cursor = servicesCollection.find({});
             //Convert to Array to use the data that curos holding
             const services = await cursor.toArray();
-            //sendi response of all services
+            //sendig response of all services
             res.send(services);
         })
 
+        //get id of service, find from DB with id and return it
+        app.get('/services/:id', async (req, res) => {
+            //get the id that need to be search
+            const id = req.params.id;
+            //set query
+            const query = { _id: new ObjectId(id) };
+            // result will have the searched item
+            const service = await servicesCollection.findOne(query);
+            //sendig response of that services
+            res.send(service);
+        })
+
+        //Get booking information from react, set to mongoDB
+        app.post('/book-service', async (req, res) => {
+            //object that need to be insert
+            const bookingInfo = req.body;
+            //Insert document into the booking_info collection
+            const result = await bookingInfoCollection.insertOne(bookingInfo);
+            //sending the response
+            res.send(result);
+        })
+
+        // return myOrders, searching by gmail
+        app.post('/my-orders', async (req, res) => {
+            // get the email
+            const email = req.body.userEmail;
+            //query that need to be search
+            const query = { userEmail: email };
+            ////Cursor is like have all myOrders
+            const cursor = bookingInfoCollection.find(query);
+            ////Convert to Array to use the data that cursor holding
+            const myOrders = await cursor.toArray();
+            //send response
+            res.send(myOrders);
+        })
+
+        // delete my orders
+        app.delete('/orders/delete', async (req, res) => {
+            // id of order, that need to be deleted
+            const id = req.body;
+            //set querry
+            const query = { _id: new ObjectId(id) };
+            //get the confirmation of delete
+            const result = await bookingInfoCollection.deleteOne(query);
+            //sending response
+            res.send(result);
+        })
+
+        //get all orders that have been placed
+        app.get('/manage-all-orders', async (req, res) => {
+            //Cursor is like have all Orders
+            const cursor = bookingInfoCollection.find({});
+            //Convert to Array to use the data that curos holding
+            const services = await cursor.toArray();
+            //sendig response of all services
+            res.send(services);
+        })
+
+        //Add a anew service, get data from react, add to db
+        app.post('/add-new-service', async (req, res) => {
+            //service that need to be insert
+            const newService = req.body;
+            //Insert document into the services collection
+            const result = await servicesCollection.insertOne(newService);
+            //sending the response
+            res.send(result);
+        })
     }
     finally {
         // await client.close();
